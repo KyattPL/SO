02de8f73bdc66bccd8b4c7c6b0d03a144aeda2aa
Kajetan Pynka <siema1234@gazeta.pl>
2020-03-19 23:56:20 +0100

    Added to .gitignore

--- cpu_scheduling_rust/target/rls/debug/deps/cpu_scheduling_rust-dc00ae545c02084d.d
+++ cpu_scheduling_rust/target/rls/debug/deps/cpu_scheduling_rust-dc00ae545c02084d.d
@@ -1,6 +1,6 @@
-c:\Users\Kyatt\Desktop\Rust\SO\cpu_scheduling_rust\target\rls\debug\deps\cpu_scheduling_rust-dc00ae545c02084d.rmeta: src\main.rs src\process.rs src\queue.rs
+c:\Users\siema\Documents\SO\SO\cpu_scheduling_rust\target\rls\debug\deps\cpu_scheduling_rust-dc00ae545c02084d.rmeta: src\main.rs src\process.rs src\queue.rs
 
-c:\Users\Kyatt\Desktop\Rust\SO\cpu_scheduling_rust\target\rls\debug\deps\cpu_scheduling_rust-dc00ae545c02084d.d: src\main.rs src\process.rs src\queue.rs
+c:\Users\siema\Documents\SO\SO\cpu_scheduling_rust\target\rls\debug\deps\cpu_scheduling_rust-dc00ae545c02084d.d: src\main.rs src\process.rs src\queue.rs
 
 src\main.rs:
 src\process.rs:
--- cpu_scheduling_rust/target/rls/debug/deps/getrandom-4241ddfd172482b1.d
+++ cpu_scheduling_rust/target/rls/debug/deps/getrandom-4241ddfd172482b1.d
@@ -1,9 +1,9 @@
-c:\Users\Kyatt\Desktop\Rust\SO\cpu_scheduling_rust\target\rls\debug\deps\getrandom-4241ddfd172482b1.rmeta: C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\lib.rs C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error.rs C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\util.rs C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error_impls.rs C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\windows.rs
+c:\Users\siema\Documents\SO\SO\cpu_scheduling_rust\target\rls\debug\deps\getrandom-4241ddfd172482b1.rmeta: C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\lib.rs C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error.rs C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\util.rs C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error_impls.rs C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\windows.rs
 
-c:\Users\Kyatt\Desktop\Rust\SO\cpu_scheduling_rust\target\rls\debug\deps\getrandom-4241ddfd172482b1.d: C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\lib.rs C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error.rs C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\util.rs C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error_impls.rs C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\windows.rs
+c:\Users\siema\Documents\SO\SO\cpu_scheduling_rust\target\rls\debug\deps\getrandom-4241ddfd172482b1.d: C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\lib.rs C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error.rs C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\util.rs C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error_impls.rs C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\windows.rs
 
-C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\lib.rs:
-C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error.rs:
-C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\util.rs:
-C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error_impls.rs:
-C:\Users\Kyatt\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\windows.rs:
+C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\lib.rs:
+C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error.rs:
+C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\util.rs:
+C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\error_impls.rs:
+C:\Users\siema\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.1.14\src\windows.rs:
